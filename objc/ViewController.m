228f92ae653a806cb1d75595e7232052289c12db
yuxiaopeng <yuxiaopeng@zuoyebang.com>
2021-08-30 19:52:40 +0800

    exact delay

--- objc/ViewController.m
+++ objc/ViewController.m
@@ -10,6 +10,27 @@
 #import <mach/mach_time.h>
 #import <pthread/pthread.h>
 
+static const uint64_t NANOS_PER_USEC = 1000ULL;
+static const uint64_t NANOS_PER_MILLISEC = 1000ULL * NANOS_PER_USEC;
+static const uint64_t NANOS_PER_SEC = 1000ULL * NANOS_PER_MILLISEC;
+
+static mach_timebase_info_data_t timebase_info;
+
+static uint64_t abs_to_nanos(uint64_t abs) {
+    return abs * timebase_info.numer / timebase_info.denom;
+}
+
+static uint64_t nanos_to_abs(uint64_t nanos) {
+    return nanos * timebase_info.denom / timebase_info.numer;
+}
+
+void example_mach_wait_until(int argc, const char * argv[]) {
+    mach_timebase_info(&timebase_info);
+    uint64_t time_to_wait = nanos_to_abs(10ULL * NANOS_PER_SEC);
+    uint64_t now = mach_absolute_time();
+    mach_wait_until(now + time_to_wait);
+}
+
 void move_pthread_to_realtime_scheduling_class(pthread_t pthread)
 {
     mach_timebase_info_data_t timebase_info;

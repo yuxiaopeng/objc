e06c994707187f836c9453a7fd86e482b087fcf7
yuxiaopeng <yuxiaopeng@zuoyebang.com>
2021-09-08 19:48:25 +0800

    SEL、IMP、Method

--- objc/ViewController.m
+++ objc/ViewController.m
@@ -60,6 +60,28 @@ void move_pthread_to_realtime_scheduling_class(pthread_t pthread)
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self SELIMPMethod];
+}
+
+/**
+ SEL:一个字符串（Char *类型），表现方法的名字
+ IMP:指向方法实现首地址的指针
+ Method:是一个结构体，包含一个SEL表示方法名、一个IMP指向函数的实现地址、一个Char*表现函数的类型（包括返回值和参数类型）
+ SEL、IMP、Method之间的关系可以这么理解：
+ 一个类（Class）持有一系列的（Method），在load类时，runtime会将所有方法的选择器（SEL）hash后映射到一个集合（NSSet）中；
+ 当需要发消息时，会根据选择器（SEL）去查找方法，找到之后，用Method结构体里的函数指针（IMP）去调用方法，这样在运行时查找selector的速度就会非常快。
+ */
+- (void)SELIMPMethod {
+    SEL selA = @selector(test);
+    SEL selB = sel_registerName("test");
+    SEL selC = NSSelectorFromString(@"test");
+    NSLog(@"%s", selB);
+    IMP methodPoint = [self methodForSelector:selC];
+    methodPoint();
+}
+
+- (void)test {
+    NSLog(@"this is test method....");
 }
 
 - (void)limit3 {
